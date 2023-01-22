fac9a894d9ae3c8b2dba042dd28a2dbfbac4ebdd
irwNd2 <irwandduadua@gmail.com>
2023-01-22 18:33:56 +0700

    fetch all products

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,9 +1,17 @@
 package productcontroller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/irwNd2/gin-rest-api/models"
+)
 
 func Fetch(c *gin.Context) {
+	var products []models.Product
 
+	models.DB.Find(&products)
+	c.JSON(http.StatusOK, gin.H{"products": products})
 }
 
 func Detail(c *gin.Context) {
